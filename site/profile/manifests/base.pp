434c832428a49243a7cc688280ca411eb2791a35
michael <mlmunro@gmail.com>
2021-12-16 11:54:07 +1100

    f

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,12 @@
 class profile::base {
+
   user {'admin':
     ensure => present
+  }
+
+  file { '/root/README':
+    ensure  => file,
+    content => 'This is a readme, some more info goes here',
+    owner   => 'root',
   }
 }
