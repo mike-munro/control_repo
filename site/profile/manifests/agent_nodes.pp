6644e5a9f9448ae7cc55edb64dbfed30455cd202
michael <mlmunro@gmail.com>
2021-12-17 08:46:23 +1100

    asd

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,6 @@
 # Contains the docker containers for puppet practice
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
+  #dockeragent::node {'web.puppet.vm':}
+  #dockeragent::node {'db.puppet.vm':}
 }
