823d1748e0313fe57ca3043de347c222ef475025
michael <mlmunro@gmail.com>
2021-12-16 13:44:02 +1100

    k

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,11 +3,9 @@ class profile::base {
 user { 'dawud':
   ensure  => 'present',
   comment => 'David Sastre Medina,,,',
-  gid     => '1001',
   groups  => ['root'],
   home    => '/home/dawud',
   shell   => '/bin/bash',
-  uid     => '1001',
 }
 
   package { 'vim':
