e6d4e6f958ebb83c1e22d405d304536928b3b9e8
michael <mlmunro@gmail.com>
2023-07-27 09:05:28 +1000

    syn

--- site/profile/manifests/app.pp
+++ site/profile/manifests/app.pp
@@ -6,9 +6,6 @@ class profile::app {
     path   => '/tmp/hosts.xml',
   }
 
-# Define the updated value for the <Server> element
-$updated_server_value = 
-
 # Apply the updated value using the xml_fragment::fragment resource
 xml_fragment::fragment { 'update_server_value':
   target     => '/tmp/server.hsconf',  # Replace with the actual path to your XML file
