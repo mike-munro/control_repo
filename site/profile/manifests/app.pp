ec95e6d9f4e7dd1b53249071cf92098dac5ef1f6
michael <mlmunro@gmail.com>
2023-07-27 07:44:43 +1000

    added xml frag

--- site/profile/manifests/app.pp
+++ site/profile/manifests/app.pp
@@ -1,5 +1,16 @@
 # not used 
 class profile::app {
-  include apt
-  include nginx
+
+xml_fragment { 'Localhost Host':
+    ensure  => 'present',
+    path    => '/tmp/hosts.xml',
+    xpath   => "/hosts/host[@ip='127.0.0.1']",
+    content => {
+        value      => 'Localhost',
+        attributes => {
+            'ip' => '127.0.0.1'
+        }
+    }
+}
+
 }
--- site/role/manifests/app_server.pp
+++ site/role/manifests/app_server.pp
@@ -1,4 +1,5 @@
 # app role
 class role::app_server {
   include profile::base
+  include profile::app
 }
