c02a3b24a73744f6a0040083b40a05abd7371466
michael <mlmunro@gmail.com>
2023-07-27 09:42:38 +1000

    xml

--- site/profile/manifests/app.pp
+++ site/profile/manifests/app.pp
@@ -1,16 +1,14 @@
 # not used 
 class profile::app {
 
-# Define the updated value for the <Server> element
-$updated_server_value = '<Server encrypted="false">new-server-value</Server>'
-
-# Apply the updated value using the xml_fragment::xml_fragment resource
-xml_fragment { 'update_server_value':
-  data     => $updated_server_value,
-  xpath    => '/EnvironmentConfiguration/PlatformDatabaseConfiguration/Server',
-  in_place => true,
-  content  => '/tmp/server.hsconf',
-}
-
-
+  xml_fragment { 'appsetting':
+    ensure  => 'present',
+    path    => '/tmp/server.hsconf',
+    xpath   => "/EnvironmentConfiguration/PlatformDatabaseConfiguration/server[@encrypted='false']",
+    content => {
+      attributes {
+        'value' => 'test'
+      }
+    }
+  }
 }
