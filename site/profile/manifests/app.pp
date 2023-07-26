9d212b0171fcf0e1a87ac10cba66a3358065303b
michael <mlmunro@gmail.com>
2023-07-27 07:50:33 +1000

    added xml frag

--- site/profile/manifests/app.pp
+++ site/profile/manifests/app.pp
@@ -3,21 +3,8 @@ class profile::app {
 
 xml_fragment { 'Hosts':
     ensure => 'present',
-    path   => 'C:/hosts.xml',
+    path   => '/tmp/hosts.xml',
     xpath  => '/hosts',
     purge  => true
 }
-
-xml_fragment { 'Localhost Host':
-    ensure  => 'present',
-    path    => '/tmp/hosts.xml',
-    xpath   => "/hosts/host[@ip='127.0.0.1']",
-    content => {
-        value      => 'Localhost',
-        attributes => {
-            'ip' => '127.0.0.1'
-        }
-    }
-}
-
 }
