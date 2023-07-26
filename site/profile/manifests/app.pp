9f6db992e78225a259fda5ae1f87ee1e1cf6c7c3
michael <mlmunro@gmail.com>
2023-07-27 09:32:47 +1000

    xml

--- site/profile/manifests/app.pp
+++ site/profile/manifests/app.pp
@@ -1,17 +1,19 @@
 # not used 
 class profile::app {
 
-  # Apply the updated value using the xml_fragment::fragment resource
-  xml_fragment { 'update_server_value':
-    ensure  => 'present',
-    path    => '/tmp/server.hsconf',
-    xpath   => '/EnvironmentConfiguration/PlatformDatabaseConfiguration/',
-    content => {
-        value      => 'Server',
-        attributes => {
-            'Server' => 'ballssssitsworking',
-        }
-    }
-  }
+# Import the xml_fragment module
+include xml_fragment
+
+# Define the updated value for the <Server> element
+$updated_server_value = '<Server encrypted="false">new-server-value</Server>'
+
+# Apply the updated value using the xml_fragment::xml_fragment resource
+xml_fragment { 'update_server_value':
+  data     => $updated_server_value,
+  xpath    => '/EnvironmentConfiguration/PlatformDatabaseConfiguration/Server',
+  in_place => true,
+  content  => '/tmp/server.hsconf',
+}
+
 
 }
