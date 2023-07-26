709197224fa24dc199860e1d4b78d175d9fee93a
michael <mlmunro@gmail.com>
2023-07-27 09:15:12 +1000

    xml

--- site/profile/manifests/app.pp
+++ site/profile/manifests/app.pp
@@ -3,10 +3,9 @@ class profile::app {
 
   # Apply the updated value using the xml_fragment::fragment resource
   xml_fragment { 'update_server_value':
-    ensure     => 'present',
-    path       => '/tmp/server.hsconf',
-    xpath      => '/EnvironmentConfiguration/PlatformDatabaseConfiguration/Server/<Server encrypted="false">new-server-value</Server>',
-    selboolean => true,
+    ensure => 'present',
+    path   => '/tmp/server.hsconf',
+    xpath  => '/EnvironmentConfiguration/PlatformDatabaseConfiguration/Server/<Server encrypted="false">new-server-value</Server>',
   }
 
 }
