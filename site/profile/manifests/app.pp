551ac26bc194c50cba893705c00b202b58d06e61
michael <mlmunro@gmail.com>
2023-07-27 07:59:26 +1000

    added xml frag

--- site/profile/manifests/app.pp
+++ site/profile/manifests/app.pp
@@ -1,20 +1,22 @@
 # not used 
 class profile::app {
 
-file {'xml':
-  ensure => file,
-  path   => '/tmp/hosts.xml',
-}
+  file {'xml':
+    ensure => file,
+    path   => '/tmp/hosts.xml',
+  }
 
 
-xml_fragment { 'Hosts':
-  ensure  => 'present',
-  path    => '/tmp/hosts.xml',
-  xpath   => "/hosts/host[@ip='127.0.0.1']",
-  content => {
+  xml_fragment { 'Hosts':
+    ensure  => 'present',
+    path    => '/tmp/hosts.xml',
+    xpath   => "/hosts/host[@ip='127.0.0.1']",
+    content => {
       value      => 'Localhost',
       attributes => {
-          'ip' => '127.0.0.1'
+        'ip' => '127.0.0.1'
       }
+    }
+  }
 }
-}
+
