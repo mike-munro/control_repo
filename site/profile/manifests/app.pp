67cbaa94c3188b52761bd63ce07afaab08a15e2d
michael <mlmunro@gmail.com>
2023-07-27 07:54:28 +1000

    added xml frag

--- site/profile/manifests/app.pp
+++ site/profile/manifests/app.pp
@@ -1,10 +1,16 @@
 # not used 
 class profile::app {
 
+file {'xml':
+  ensure => file,
+  path   => '/tmp/hosts.xml',
+}
+
+
 xml_fragment { 'Hosts':
-    ensure => 'present',
-    path   => '/tmp/hosts.xml',
-    xpath  => '/hosts',
-    purge  => true
+  ensure => 'present',
+  path   => '/tmp/hosts.xml',
+  xpath  => '/hosts',
+  purge  => true
 }
 }
