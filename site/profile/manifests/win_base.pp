fe6fa1b97040698c933f4abf86b385175c6ea7f8
michael <mlmunro@gmail.com>
2023-07-30 19:57:20 +1000

    saf

--- site/profile/manifests/win_base.pp
+++ site/profile/manifests/win_base.pp
@@ -8,12 +8,30 @@ class profile::win_base {
     content => 'This is a readme, some more info goes here',
   }
 
-  dsc { 'iis':
-    resource_name => 'WindowsFeature',
-    module        => 'PSDesiredStateConfiguration',
-    properties    => {
-      ensure => 'present',
-      name   => 'Web-Server',
-    },
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
+
+  iis_feature { $iis_features:
+    ensure => 'present',
+  }
+
+# Delete the default website to prevent a port binding conflict.
+  iis_site { 'Default Web Site':
+    ensure  => absent,
+    require => Iis_feature['Web-WebServer'],
+  }
+
+  iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'DefaultAppPool',
+    require         => [
+      File['minimal'],
+      Iis_site['Default Web Site']
+    ],
+  }
+
+  file { 'minimal':
+    ensure => 'directory',
+    path   => 'c:\\inetpub\\minimal',
   }
 }
