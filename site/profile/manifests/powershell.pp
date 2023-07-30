dbe021f1a090c3588ee61d1400f187b258112c4a
michael <mlmunro@gmail.com>
2023-07-30 21:59:43 +1000

    asdf

--- site/profile/manifests/powershell.pp
+++ site/profile/manifests/powershell.pp
@@ -1,13 +1,13 @@
 class profile::powershell {
   exec { 'install_nuget_provider':
     command  => 'powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force"',
-    provider => 'shell',
+    provider => 'powershell',
     creates  => 'C:/Program Files/PackageManagement/ProviderAssemblies/NuGet',
   }
 
   exec { 'import_nuget_provider':
-    command  => 'powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command "Import-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force"',
-    provider => 'shell',
+    command  => 'Import-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force',
+    provider => 'powershell',
     require  => Exec['install_nuget_provider'],
   }
 }
