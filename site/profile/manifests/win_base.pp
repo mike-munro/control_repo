bd05888a6f926c060e852da2ae66e32901d7ff97
michael <mlmunro@gmail.com>
2023-07-30 19:09:00 +1000

    asdf

--- site/profile/manifests/win_base.pp
+++ site/profile/manifests/win_base.pp
@@ -2,8 +2,8 @@
 class profile::win_base {
   file { 'c://temp':
     ensure => directory,
-    mode   => '0660',
-    group  => 'vagrant',
+    mode   => '7770',
+    group  => 'Administrators',
   }
   file { 'c://temp/README.txt':
     ensure  => file,
