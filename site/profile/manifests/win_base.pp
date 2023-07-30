f572b3c3beb9d73cd39b8a2ca4de0a890944fedb
michael <mlmunro@gmail.com>
2023-07-30 19:32:32 +1000

    asd

--- site/profile/manifests/win_base.pp
+++ site/profile/manifests/win_base.pp
@@ -7,4 +7,13 @@ class profile::win_base {
     ensure  => file,
     content => 'This is a readme, some more info goes here',
   }
+
+  dsc { 'iis':
+    resource_name => 'WindowsFeature',
+    module        => 'PSDesiredStateConfiguration',
+    properties    => {
+      ensure => 'present',
+      name   => 'Web-Server',
+    },
+  }
 }
