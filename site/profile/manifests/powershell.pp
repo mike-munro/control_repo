b8b89cc5273a482e8295c0b66c1a7a591518a59e
michael <mlmunro@gmail.com>
2023-08-03 08:07:26 +1000

    asf

--- site/profile/manifests/powershell.pp
+++ site/profile/manifests/powershell.pp
@@ -1,3 +1,4 @@
+#
 class profile::powershell {
   # Install NuGet Package Provider
   exec { 'install_nuget_provider':
@@ -11,5 +12,20 @@ class profile::powershell {
     command  => 'Install-Module -Name NuGet -Force -SkipPublisherCheck',
     provider => 'powershell',
     creates  => 'C:/Program Files/PackageManagement/ProviderAssemblies/NuGet/2.8.5.208',
+  }
+
+  $modules_to_install = [
+    'carbon.iis',
+    'carbon',  # Add more module names as needed
+    'Puppet.Dsc',
+  ]
+
+  # Install PowerShell modules using iteration
+  $modules_to_install.each |$module| {
+    exec { "install_${module}_module":
+      command  => "Install-Module -Name ${module} -Force -SkipPublisherCheck",
+      provider => 'powershell',
+      creates  => "C:/Program Files/PackageManagement/ProviderAssemblies/NuGet/2.8.5.208/${module}",
+    }
   }
 }
