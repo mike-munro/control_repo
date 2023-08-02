27026e222a803d2d66c2a94573801770343d7e9d
michael <mlmunro@gmail.com>
2023-08-03 08:13:20 +1000

    asdf

--- site/profile/manifests/powershell.pp
+++ site/profile/manifests/powershell.pp
@@ -14,12 +14,6 @@ class profile::powershell {
     creates  => 'C:/Program Files/PackageManagement/ProviderAssemblies/NuGet/2.8.5.208',
   }
 
-  $modules_to_install = [
-    'carbon.iis',
-    'carbon',  # Add more module names as needed
-    'Puppet.Dsc',
-  ]
-
   # Install PowerShell modules using iteration
   $modules_to_install.each |$module| {
     exec { "install_${module}_module":
